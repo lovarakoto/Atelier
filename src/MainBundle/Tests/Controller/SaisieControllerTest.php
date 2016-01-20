762315f883de9aa52c75248280a2c8bd7e884c26
lovarakoto <lovarakoto1@gmail.com>
2016-01-20 09:09:03 +0100

    test sur accueil

--- src/MainBundle/Tests/Controller/SaisieControllerTest.php
+++ src/MainBundle/Tests/Controller/SaisieControllerTest.php
@@ -4,14 +4,14 @@ namespace MainBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class SasieControllerTest extends WebTestCase
 {
     public function testIndex()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/saisie');
 
-        $this->assertTrue($crawler->filter('html:contains("ATELIER")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("Saisie")')->count() > 0);
     }
 }
